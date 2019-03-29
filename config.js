020c96ca798049487371df9cb254b48417473f0c
Alli Pao <allipao@Allis-MacBook-Pro.local>
2019-03-29 18:10:56 -0400

    WORKING - just need to style

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const config = {
     pword: "root", // windows users leave this blank
     host: "localhost",
     port: "8889", // windows users - sql port is 3306
-    database: "db_favethings"
+    database: "db_favouritethings"
 }
 
 module.exports = config;
--- db_marvelcharacters.sql
+++ db_marvelcharacters.sql
@@ -1,17 +1,23 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Generation Time: Mar 29, 2019 at 07:05 PM
--- Server version: 5.7.23
--- PHP Version: 7.2.10
+-- Host: localhost
+-- Generation Time: Mar 20, 2019 at 01:24 PM
+-- Server version: 5.6.38
+-- PHP Version: 7.2.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
--- Database: `db_marvelcharacters`
+-- Database: `marvelcharacters`
 --
 
 -- --------------------------------------------------------
@@ -57,3 +63,7 @@ ALTER TABLE `hero`
 --
 ALTER TABLE `hero`
   MODIFY `id` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
